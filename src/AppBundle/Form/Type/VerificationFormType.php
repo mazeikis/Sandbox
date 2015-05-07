96b2739b98ec0f7b2c827fb9dd7f4a20c7cf3aa7
obscenewordsandspaces <f.mazeikis@gmail.com>
2015-05-07 18:23:08 +0100

    Form

--- src/AppBundle/Form/Type/VerificationFormType.php
+++ src/AppBundle/Form/Type/VerificationFormType.php
@@ -4,6 +4,8 @@ namespace AppBundle\Type\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class VerificationFormType extends AbstractType
 {
@@ -13,8 +15,12 @@ class VerificationFormType extends AbstractType
 	}
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-		$builder->add('username', 'text', array('label'  => 'Your username: ', 'required' => true))
-		    ->add('reset password', 'submit');
+		$builder->add('plainPassword', 'repeated', array(
+	               'first_name'  => 'password',
+	               'second_name' => 'confirm',
+	               'type'        => 'password',
+	               'constraints' => new Length(array('min' => 6, 'max' => 4096), new NotBlank)))
+		    ->add('save', 'submit', array('label' => 'Upload'));
 	}
 	public function setDefaultOptions(OptionsResolverInterface $resolver)
 	{
