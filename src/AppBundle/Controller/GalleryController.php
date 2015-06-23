8512aa894264a062b5ff164421686ba6643a5135
obscenewordsandspaces <Feliksas@Feliksass-Air.default>
2015-06-24 00:32:20 +0100

    Refactoring

--- src/AppBundle/Controller/GalleryController.php
+++ src/AppBundle/Controller/GalleryController.php
@@ -59,19 +59,21 @@ class GalleryController extends Controller
     }//end indexAction()
 
 
-    public function imageAction($imageId)
+    public function imageAction($id)
     {
-        $user = $this->getUser();
-        $em    = $this->getDoctrine()->getManager();
-        $image = $em->getRepository('AppBundle:Image')->findOneBy(array('id' => $imageId));
+        $user          = $this->getUser();
+        $entityManager = $this->getDoctrine()->getManager();
+        $image         = $entityManager->getRepository('AppBundle:Image')->findOneBy(array('id' => $id));
+
         if ($image === null) {
             throw $this->createNotFoundException('No image with id '.$imageId);
         }
         if ($user === null) {
             throw $this->createNotFoundException(('no user'));
         }
-        $votes = $em->getRepository('AppBundle:Vote')->countVotes($image);
-        $test = $em->getRepository('AppBundle:Vote')->checkForVote($user, $image);
+
+        $votes = $entityManager->getRepository('AppBundle:Vote')->countVotes($image);
+        $test = $entityManager->getRepository('AppBundle:Vote')->checkForVote($user, $image);
         return $this->render('AppBundle:Twig:image.html.twig', array('title' => 'sandbox|image', 'image' => $image, 'test' =>$test, 'votes' =>$votes));
 
     }//end imageAction()
@@ -100,9 +102,9 @@ class GalleryController extends Controller
                   ->setOwner($user);
             $imageDir = $this->get('kernel')->getRootDir().'/../web/images'.$this->getRequest()->getBasePath();
             $data['file']->move($imageDir, $randomFileName.'.'.$data['file']->getClientOriginalExtension());
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($image);
-            $em->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($image);
+            $entityManager->flush();
             $this->addFlash('success', 'Image uploaded!');
             return $this->redirectToRoute('_gallery');
         } else {
@@ -117,8 +119,8 @@ class GalleryController extends Controller
 
     public function imageEditAction(Request $request, $imageId)
     {
-        $em    = $this->getDoctrine()->getManager();
-        $image = $em->getRepository('AppBundle:Image')->findOneBy(array('id' => $imageId));
+        $entityManager    = $this->getDoctrine()->getManager();
+        $image = $entityManager->getRepository('AppBundle:Image')->findOneBy(array('id' => $imageId));
 
         if ($this->get('security.authorization_checker')->isGranted('edit', $image) === false) {
             throw new AccessDeniedException('Unauthorised access!');
@@ -136,7 +138,7 @@ class GalleryController extends Controller
         if ($form->isValid() === true) {
             $data = $form->getData();
             $image->setTitle($data['title'])->setDescription($data['description'])->setUpdated(new \Datetime());
-            $em->flush();
+            $entityManager->flush();
             return $this->redirectToRoute('_image', array('id' => $imageId));
         }
 
@@ -159,10 +161,10 @@ class GalleryController extends Controller
         } else {
                 $em->remove($image);
                 $em->flush();
-                $fs       = new Filesystem();
+                $fileSystem       = new Filesystem();
                 $imageDir = $this->get('kernel')->getRootDir().'/../web/images'.$this->getRequest()->getBasePath();
-                $fs->remove($imageDir.$image->getFileName().'.'.$image->getExtension());
-                $fs->remove($imageDir.'/cache/thumb/'.$image->getFileName().'.'.$image->getExtension());
+                $fileSystem->remove($imageDir.$image->getFileName().'.'.$image->getExtension());
+                $fileSystem->remove($imageDir.'/cache/thumb/'.$image->getFileName().'.'.$image->getExtension());
                 $request->getSession()
                     ->getFlashBag()
                     ->add('success', 'Image deleted!');
@@ -177,30 +179,20 @@ class GalleryController extends Controller
         $voteValue     = $request->request->get('voteValue');
         $imageId       = $request->request->get('id');
         $entityManager = $this->getDoctrine()->getManager();
-        $image         = $em->getRepository('AppBundle:Image')->findOneBy(array('id' => $imageId));
+        $image         = $entityManager->getRepository('AppBundle:Image')->findOneBy(array('id' => $imageId));
         $user          = $this->getUser();
         $voteWhiteList = array(-1, 1);
 
-        if (in_array($voteValue, $voteWhiteList) !== false) {
-            return $this->redirectToRoute('_home');
-        }
-
         $voteCheck = $entityManager->getRepository('AppBundle:Vote')->findOneBy(array('user' => $user, 'image' => $image));
-        if ($voteCheck !== null) {
-            throw new AccessDeniedException('Already voted!');
-        }
 
-        if ($this->get('security.authorization_checker')->isGranted('vote', $image, $user) === false) {
+        if ($this->get('security.authorization_checker')->isGranted('vote', $image, $user) === false || $voteCheck !== null || in_array($voteValue, $voteWhiteList) === false) {
             throw new AccessDeniedException('Unauthorised access to voting!');
         }
 
-        if ($image === false) {
-                throw $this->createNotFoundException('No image with id '.$imageId);
-        }
         $vote = new Vote();
         $vote->setImage($image)->setUser($user)->setVote($voteValue);
-        $em->persist($vote);
-        $em->flush();
+        $entityManager->persist($vote);
+        $entityManager->flush();
         $request->getSession()
                 ->getFlashBag()
                 ->add('success', 'Vote recorded, thanks!');
