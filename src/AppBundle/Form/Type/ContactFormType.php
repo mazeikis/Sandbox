e3cf07467a95e99f37fd9097f23fd782169e4f6b
obscenewordsandspaces <Feliksas@Feliksass-Air.default>
2015-07-06 14:39:08 +0100

    Front end fixes

--- src/AppBundle/Form/Type/ContactFormType.php
+++ src/AppBundle/Form/Type/ContactFormType.php
@@ -20,7 +20,7 @@ class ContactFormType extends AbstractType
     {
         $builder ->add('from', 'email', array('label' => 'From', 'required' => true))
         ->add('subject', 'text', array('label' => 'Subject', 'required' => true))
-        ->add('message', 'textarea', array('label' => 'Message:', 'required' => true))
+        ->add('message', 'textarea', array('label' => 'Message:', 'required' => true, 'attr' => array('class' => 'contact-form-textarea')))
         ->add('Send', 'submit');
 
     }//end buildForm()
