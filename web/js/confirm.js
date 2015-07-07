e6d08d0934ccc6ab2321d2cbdf19e7e60010fceb
obscenewordsandspaces <Feliksas@Feliksass-Air.default>
2015-07-08 00:57:51 +0100

    Image delete confirmation

--- web/js/confirm.js
+++ web/js/confirm.js
@@ -1,7 +1,6 @@
 $('#confirmDelete').on('show.bs.modal', function (e) {
       var form = $(e.relatedTarget).closest('form');
       $(this).find('.modal-footer #confirm').data('form', form);
-      alert('test');
   });
 
 $('#confirmDelete').find('.modal-footer #confirm').on('click', function(){
