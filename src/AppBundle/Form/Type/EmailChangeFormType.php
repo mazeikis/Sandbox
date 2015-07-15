744d5e032e6f1026c4c53743b347cb26260acaf5
obscenewordsandspaces <Feliksas@Feliksass-Air.default>
2015-07-15 10:25:20 +0100

    FormType fixes

--- src/AppBundle/Form/Type/EmailChangeFormType.php
+++ src/AppBundle/Form/Type/EmailChangeFormType.php
@@ -4,7 +4,6 @@ namespace AppBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class EmailChangeFormType extends AbstractType
@@ -20,7 +19,7 @@ class EmailChangeFormType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('email', 'email', array('label' => 'Your email address: ', 'required' => true))
+        $builder->add('email', 'email', array('label' => 'Your email address: ', 'required' => true, 'constraints' => new NotBlank))
                 ->add('save', 'submit', array('label' => 'Register'))
                 ->add('Cancel', 'button', array('attr' => array('data-dismiss' => 'modal')));
 
