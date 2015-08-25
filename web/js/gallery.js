05f5f4864b248d6fc0057200bdd4bbfec537a5af
obscenewordsandspaces <Feliksas@Feliksass-Air.default>
2015-08-25 22:47:18 +0100

    Bugfix

--- web/js/gallery.js
+++ web/js/gallery.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-            $(".pagerfanta").on('click', 'a', function(event) {
+            $(document).on('click', '.pagination a', function(event) {
                 event.preventDefault();
                 var link = $(this).attr('href');
                 jQuery.get(link, function(result){
-                    $(".content").hide().stop().fadeOut('fast'); 
-                    $(".content").empty();
-                    $(".content").html($(result)).stop().fadeIn('fast');
+                    $(".content").detach().fadeOut('fast');
+                    $(".content-wrapper").append($(result)).fadeIn('fast');
                 });
-            })
+            });
         });
