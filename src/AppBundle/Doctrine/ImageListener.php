9cb297ee76f649ec5789948082da2124461c2435
obscenewordsandspaces <Feliksas@Feliksass-Air.default>
2015-09-02 11:51:38 +0100

    Doctrine Update And Bugfix

--- src/AppBundle/Doctrine/ImageListener.php
+++ src/AppBundle/Doctrine/ImageListener.php
@@ -12,18 +12,16 @@ class ImageListener
     {
         $entity = $args->getEntity();
         if ($entity instanceof Image) {
-            $this->handleEvent($entity);
             $entity->setCreated(new \DateTime());
         }
 
     }
 
 
-    private function preUpdate(Image $image)
+    private function preUpdate(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
         if ($entity instanceof Image) {
-            $this->handleEvent($entity);
             $entity->setUpdated(new \DateTime());
         }
 
