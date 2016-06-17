da5c4ddafe448180266a68ff4b53bc211257e4bd
mazeikis <f.mazeikis@gmail.com>
2016-06-17 11:22:23 +0100

    Testing after 3.0
    Autoloading correct namespace, adding ContainerAwareInterface to data
    fixtures, in order to encode passwords for testing purposes.

--- src/AppBundle/DataFixtures/ORM/LoadTestData.php
+++ src/AppBundle/DataFixtures/ORM/LoadTestData.php
@@ -4,16 +4,28 @@ namespace AppBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use AppBundle\Entity\Image;
 use AppBundle\Entity\User;
 
-class LoadTestData implements FixtureInterface
+class LoadTestData implements FixtureInterface, ContainerAwareInterface
 {
+    private $container;
+    /**
+    * @var ContainerInterface
+    */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
     public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setUserName('User');
-        $user->setPlainPassword('test');
+        $encoder  = $this->container->get('security.password_encoder');
+        $password = $encoder->encodePassword($user, 'password');
+        $user->setPassword($password);
         $user->setFirstName('Jon');
         $user->setLastName('Doe');
         $user->setEmail('jon.doe@test.com');
@@ -26,6 +38,8 @@ class LoadTestData implements FixtureInterface
         $image->setTitle('Test Image Title 1');
         $image->setDescription('Test Image 1 description');
         $image->setResolution('test X test');
+        $image->setCreated(new \DateTime());
+        $image->setUpdated(new \DateTime());
         $image->setOwner($user);
 
 
